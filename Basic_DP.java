6e419fb64f823878162900ac169e6a51cdb8f713
ravisravinder <115361406+ravisravinder@users.noreply.github.com>
2025-05-28 23:26:37 +0530

    Update Basic_DP.java

--- Basic_DP.java
+++ Basic_DP.java
@@ -1,4 +1,4 @@
-1 .public class FibonacciMemoizationArray {
+1 .public class FibonacciMemoizationArray {  //TopDOwn ===Recursion ==Memoization
 
     public static int fibonacci(int n, int[] memo) {
         if (n <= 1) return n;
@@ -18,6 +18,8 @@
     }
 }
 
+// Time Complexity	O(n)
+// Space Complexity	O(n)
 ========================================
 import java.util.HashMap;
 import java.util.Map;
@@ -47,4 +49,70 @@ public class FibonacciMemoization {
     }
 }
 
+// Time Complexity	O(n)
+// Space Complexity	O(n)  [O(n) for recurstion+o(n) for array/map]
 ===========================  
+
+   //Tabulation
+
+    public class FibonacciTabulation {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int count = 5;
+        for (int i = 0; i < count; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
+
+TC: O(n)
+SC: o(n)    
+=================================================================
+
+    Optimized
+
+    public class FibonacciOptimized {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+
+        int prev2 = 0; // Fibonacci(0)
+        int prev1 = 1; // Fibonacci(1)
+        int current = 0;
+
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+
+    public static void main(String[] args) {
+        int count = 5;
+        for (int i = 0; i < count; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
+
+===============================================TUF-1 end-------------------------------------------------------------------------
+    
+    
+
+    
